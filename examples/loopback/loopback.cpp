f407f3c645e7caa9d4db502589fef514a8194d70
Sean Shahkarami <sean.shahkarami@gmail.com>
2018-09-26 13:50:28 -0500

    changed var name in example

--- examples/loopback/loopback.cpp
+++ examples/loopback/loopback.cpp
@@ -3,38 +3,38 @@
 using namespace Waggle;
 
 int main() {
-    LoopbackIO<1024> loopback;
+    LoopbackIO<1024> io;
     Sensorgram<64> s;
 
     s.sensorID = 1;
     s.parameterID = 0;
     s.SetString("starting!");
-    s.Pack(loopback);
+    s.Pack(io);
 
     s.sensorID = 2;
     s.parameterID = 0;
     s.SetUint(341);
-    s.Pack(loopback);
+    s.Pack(io);
 
     s.sensorID = 3;
     s.parameterID = 0;
     s.sensorInstance = 1;
     s.SetInt(-12345);
-    s.Pack(loopback);
+    s.Pack(io);
 
     s.sensorID = 4;
     s.parameterID = 3;
     s.sensorInstance = 1;
     s.SetUint(9999);
-    s.Pack(loopback);
+    s.Pack(io);
 
     s.sensorID = 9;
     s.parameterID = 3;
     s.sensorInstance = 2;
     s.SetString("hello!");
-    s.Pack(loopback);
+    s.Pack(io);
 
-    while (s.Unpack(loopback)) {
+    while (s.Unpack(io)) {
         printf("sensor id: %d\n", s.sensorID);
         printf("parameter id: %d\n", s.parameterID);
 
