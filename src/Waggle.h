8193758b4cc1e3e216e8ca81942c71c343e42cd1
Sean Shahkarami <sean.shahkarami@gmail.com>
2018-10-08 15:39:04 -0500

    fixed include guard

--- src/Waggle.h
+++ src/Waggle.h
@@ -1,7 +1,12 @@
+#ifndef __H_WAGGLE__
+#define __H_WAGGLE__
+
 #if defined(ARDUINO_ARCH_AVR)
   #include "waggle/waggle_arduino.h"
 #elif defined(ARDUINO_ARCH_SAM)
   #include "waggle/waggle_arduino.h"
 #else
   #include "waggle/waggle_native.h"
+#endif
+
 #endif
