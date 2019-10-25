63d513810df7282520ec352d9c0c96200cf27dec
Sean Shahkarami <sean.shahkarami@gmail.com>
2019-10-25 16:40:17 -0500

    added longer base64 test case

--- tests/waggle_test.cpp
+++ tests/waggle_test.cpp
@@ -24,10 +24,27 @@ void test_simple() {
   e.close();
 }
 
+std::string wiki_input =
+    "Man is distinguished, not only by his reason, but by this singular "
+    "passion from other animals, "
+    "which is a lust of the mind, that by a perseverance of delight in the "
+    "continued and indefatigable "
+    "generation of knowledge, exceeds the short vehemence of any carnal "
+    "pleasure.";
+
+std::string wiki_expect =
+    "TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1dCBieS"
+    "B0aGlzIHNpbmd1bGFyIHBhc3Npb24gZnJvbSBvdGhlciBhbmltYWxzLCB3aGljaCBpcyBh"
+    "IGx1c3Qgb2YgdGhlIG1pbmQsIHRoYXQgYnkgYSBwZXJzZXZlcmFuY2Ugb2YgZGVsaWdodC"
+    "BpbiB0aGUgY29udGludWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdlbmVyYXRpb24gb2Yga25v"
+    "d2xlZGdlLCBleGNlZWRzIHRoZSBzaG9ydCB2ZWhlbWVuY2Ugb2YgYW55IGNhcm5hbCBwbG"
+    "Vhc3VyZS4";
+
 int main() {
   test_encode("", "");
   test_encode("A", "QQ");
   test_encode("AZ", "QVo");
   test_encode("AZQ", "QVpR");
   test_encode("1234", "MTIzNA");
+  test_encode(wiki_input, wiki_expect);
 }
