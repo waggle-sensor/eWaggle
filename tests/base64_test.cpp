5e827c1c2889c4388d1491792cddccb2986a7b42
Sean Shahkarami <sean.shahkarami@gmail.com>
2019-10-25 15:50:05 -0500

    adding base64 tools

---
+++ tests/base64_test.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <sstream>
+#include "Waggle.h"
+
+void test_encode(std::string input, std::string expect) {
+  std::stringstream s;
+
+  base64encoder<std::stringstream> e(s);
+  e.write(input.c_str(), input.length());
+  e.close();
+
+  if (s.str() != expect) {
+    std::cout << "error: encode(" << input << ") -> " << s.str()
+              << " != " << expect << std::endl;
+  }
+}
+
+void test_simple() {
+  base64encoder<typeof(std::cout)> e(std::cout);
+  e.write("1234", 4);
+  e.close();
+}
+
+int main() {
+  test_encode("", "");
+  test_encode("A", "QQ");
+  test_encode("AZ", "QVo");
+  test_encode("1234", "MTIzNA");
+}
