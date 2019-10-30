2a02eb69e5483c14c64b912db613d531fbbd666c
Sean Shahkarami <sean.shahkarami@gmail.com>
2019-10-30 13:48:05 -0500

    clean up base64 encoder and working on base64 decoder

--- src/base64.h
+++ src/base64.h
@@ -2,71 +2,106 @@
 #define __H_WAGGLE_BASE64__
 
 const byte base64[] =
-    ("ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-     "abcdefghijklmnopqrstuvwxyz"
-     "0123456789+/");
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    "abcdefghijklmnopqrstuvwxyz"
+    "0123456789+/";
 
 struct base64_encoder : public writer, public closer {
   writer &w;
+  byte buf[3];
+  int nbuf;
+  byte out[4];
+  bool err;
 
-  bool closed;
-  int remain;
-  byte b3[3];
-
-  base64_encoder(writer &w) : w(w) {
-    remain = 0;
-    closed = false;
-  }
+  base64_encoder(writer &w) : w(w), nbuf(0), err(false) {}
 
   void close() {
-    if (closed) {
-      return;
-    }
-
-    closed = true;
-
-    byte b4[4];
-
-    if (remain == 1) {
-      unsigned int x = (b3[0] << 16);
-      b4[0] = base64[(x >> 18) & 63];
-      b4[1] = base64[(x >> 12) & 63];
-      b4[2] = '=';
-      b4[3] = '=';
-      w.write(b4, 4);
-    } else if (remain == 2) {
-      unsigned int x = (b3[0] << 16) | (b3[1] << 8);
-      b4[0] = base64[(x >> 18) & 63];
-      b4[1] = base64[(x >> 12) & 63];
-      b4[2] = base64[(x >> 6) & 63];
-      b4[3] = '=';
-      w.write(b4, 4);
+    if (!err && nbuf > 0) {
+      encode();
     }
   }
 
   int write(const byte *b, int n) {
-    if (closed) {
-      return 0;
-    }
-
     for (int i = 0; i < n; i++) {
-      b3[remain++] = b[i];
+      buf[nbuf++] = b[i];
 
-      if (remain == 3) {
-        remain = 0;
-
-        byte b4[4];
-        int x = (b3[0] << 16) | (b3[1] << 8) | b3[2];
-        b4[0] = base64[(x >> 18) & 63];
-        b4[1] = base64[(x >> 12) & 63];
-        b4[2] = base64[(x >> 6) & 63];
-        b4[3] = base64[x & 63];
-        w.write(b4, 4);
+      if (nbuf == 3) {
+        encode();
       }
     }
 
-    // ah...we'll have to compute an estimate somewhere..
     return n;
+  }
+
+  void encode() {
+    if (nbuf == 1) {
+      unsigned int x = (buf[0] << 16);
+      out[0] = base64[(x >> 18) & 63];
+      out[1] = base64[(x >> 12) & 63];
+      out[2] = '=';
+      out[3] = '=';
+      w.write(out, 4);
+    } else if (nbuf == 2) {
+      unsigned int x = (buf[0] << 16) | (buf[1] << 8);
+      out[0] = base64[(x >> 18) & 63];
+      out[1] = base64[(x >> 12) & 63];
+      out[2] = base64[(x >> 6) & 63];
+      out[3] = '=';
+      w.write(out, 4);
+    } else if (nbuf == 3) {
+      unsigned int x = (buf[0] << 16) | (buf[1] << 8) | buf[2];
+      out[0] = base64[(x >> 18) & 63];
+      out[1] = base64[(x >> 12) & 63];
+      out[2] = base64[(x >> 6) & 63];
+      out[3] = base64[x & 63];
+      w.write(out, 4);
+    } else {
+      err = true;
+    }
+
+    nbuf = 0;
+  }
+};
+
+struct base64_decoder : public reader {
+  reader &r;
+  byte buf[4];
+  byte out[3];
+  int remain;
+  bool err;
+
+  base64_decoder(reader &r) : r(r), remain(0), err(false) {}
+
+  int read(byte *s, int n) {
+    for (int i = 0; i < n; i++) {
+      buf[remain++] = s[i];
+
+      // this is wrong...= needs to signal decode time
+      // accumulate 4 byte block for decoding
+      if (remain < 4) {
+        continue;
+      }
+
+      remain = 0;
+    }
+
+    return 0;
+  }
+
+  // TODO replace with more efficient search
+  int base64_value(byte x) {
+    if (x == '=') {
+      return 0;
+    }
+
+    for (int i = 0; i < 64; i++) {
+      if (x == base64[i]) {
+        return i;
+      }
+    }
+
+    err = true;
+    return 0;
   }
 };
 
